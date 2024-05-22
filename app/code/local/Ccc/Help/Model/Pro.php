10fa99315d11fcdcfea09b69dc54682c4982c46b
mayanksaini9737 <113924903+mayanksaini9737@users.noreply.github.com>
2024-05-22 09:55:17 +0530

    model override of product

---
+++ app/code/local/Ccc/Help/Model/Pro.php
@@ -0,0 +1,11 @@
+<?php 
+
+class Ccc_Help_Model_Pro extends Mage_Catalog_Model_Product
+{
+    // model override and change final price
+    // public function getFinalPrice($qty=null)
+    // {
+    //     return 7;
+    // }
+}
+?>
